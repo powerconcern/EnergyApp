e74a9ea5a3ba3a24aaacb70ca50a097665901687
Tommy Ekh <tommy.ekh@outlook.com>
2019-07-21 23:45:26 +0200

    Added pages for config

---
+++ Pages/Configuration/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EnergyApp.Data;
+
+namespace EnergyApp.Pages_Configuration
+{
+    public class CreateModel : PageModel
+    {
+        private readonly EnergyApp.Data.ApplicationDbContext _context;
+
+        public CreateModel(EnergyApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Configuration Configuration { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Configurations.Add(Configuration);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Configuration/Edit.cshtml.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EnergyApp.Data;
+
+namespace EnergyApp.Pages_Configuration
+{
+    public class EditModel : PageModel
+    {
+        private readonly EnergyApp.Data.ApplicationDbContext _context;
+
+        public EditModel(EnergyApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Configuration Configuration { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Configuration = await _context.Configurations.FirstOrDefaultAsync(m => m.ConfigurationID == id);
+
+            if (Configuration == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Configuration).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConfigurationExists(Configuration.ConfigurationID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ConfigurationExists(int id)
+        {
+            return _context.Configurations.Any(e => e.ConfigurationID == id);
+        }
+    }
+}
