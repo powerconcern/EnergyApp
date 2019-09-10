17c86d0b8d88a54330d10b5d96c53e55a4087df3
Tommy Ekh <tommy.ekh@coop.se>
2019-09-10 20:57:42 +0200

    Added ID

--- RelationTest/Model/CMCAssign.cs
+++ RelationTest/Model/CMCAssign.cs
@@ -4,10 +4,11 @@ namespace EnergyApp.Data
 {
     public class CMCAssign
     {
+        public int ID { get; set; }
         public int MeterID { get; set; }
         public int ChargerID { get; set; }
         public int CusomerID { get; set; }
-        public Meter Meters { get; set; }
+        public Meter Meter { get; set; }
         public Charger Charger { get; set; }
         public Customer Customer { get; set; }
     }
